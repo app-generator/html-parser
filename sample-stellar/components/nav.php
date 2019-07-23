bac46f83bf054d4255362c775a6008c87e3b961c
Adi Chirilov <developer@rosoftware.ro>
2019-07-23 13:48:20 +0300

    Added sample - HTML5Up Stellar

---
+++ sample-stellar/components/nav.php
@@ -0,0 +1,24 @@
+<nav id="nav">
+ <ul>
+  <li>
+   <a class="active newclass" href="https://appseed.us/html-parser">
+    <?php echo $var_1?>
+   </a>
+  </li>
+  <li>
+   <a href="#first">
+    <?php echo $var_2?>
+   </a>
+  </li>
+  <li>
+   <a href="#second">
+    <?php echo $var_3?>
+   </a>
+  </li>
+  <li>
+   <a href="#cta">
+    <?php echo $var_4?>
+   </a>
+  </li>
+ </ul>
+</nav>
